9830b59674f8678f069e4e0c47cad978d2b08efa
4elovel <marcynakvova007@gmail.com>
2023-04-18 22:36:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write words with how many letters do you want to print:");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            List<string> list =new List<string>()
+            {
+                "Hi",
+                "Hello",
+                "How",
+                "are",
+                "you",
+                "?",
+                " ",
+                "I",
+                "can",
+                "say",
+                "that",
+                "you",
+                "are",
+                "so",
+                "pretty",
+                "and",
+                "handsome",
+                ".",
+                " ",
+                "Do",
+                "You",
+                "want",
+                "to",
+                "dance",
+                "with",
+                "me",
+                ",",
+                "please",
+                "?"
+            };
+            var linq = from i in list
+                       where i.Length == size
+                       orderby list
+                       select i;
+            Console.WriteLine($"Words with {size} letters:");
+            foreach ( var i in linq ) { Console.WriteLine(i); }
+            Console.ReadKey();
+        }
+    }
+}
